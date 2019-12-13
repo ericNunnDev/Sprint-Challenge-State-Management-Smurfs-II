f1d10da279555523062f24662e989161cea3a5a8
lowlyHero <e.nunn1009@gmail.com>
2019-12-13 12:25:50 -0500

    Redux store connected to index and appJS

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getSmurfs } from './store/actions';
 import Smurf from './components/Smurf';
 import SmurfList from './components/SmurfList';
 import SmurfForm from './components/SmurfForm';
 import "./App.css";
 
 class App extends Component {
+  state = {
+    smurf: []
+  }
+
   render() {
     return ( 
       <div className='container'>
@@ -15,4 +21,6 @@ class App extends Component {
   }
 }
  
-export default App;
+const mapStateToProps = state => ({ smurfs: state.smurf })
+
+export default connect(mapStateToProps, { getSmurfs })(App);
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,5 +1,24 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Middleware
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+// Redux
+import {createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducers';
+
+// App
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root'));
+    
